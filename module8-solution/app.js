5b3ec7fa7515a45809ee4698e2a2a3de96e1cb0b
Miles Stewart <mstewa33@jhu.edu>
2018-04-09 00:16:24 -0400

    Updates to HW8

--- module8-solution/app.js
+++ module8-solution/app.js
@@ -3,31 +3,60 @@
 
     angular.module('NarrowItDownApp', [])
         .controller('NarrowItDownController', NarrowItDownController)
-        .service('MenuSearchService', ShoppingListCheckOffService)
+        .service('MenuSearchService', MenuSearchService)
         .directive('foundItems', FoundItems);
 
     NarrowItDownController.$inject = ['MenuSearchService'];
     MenuSearchService.$inject = ['$http'];
 
     function NarrowItDownController(MenuSearchService){
-        var found;
+        var controller = this;
+        controller.found = [];
+
+        controller.getMatchedMenuItems = function(searchTerm){
+            var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+            promise.then(function(result){
+                controller.found = result;
+            });
+        };
+
+        controller.removeItem = function(index){
+            controller.found.splice(index, 1);
+        }
+
     };
 
     function MenuSearchService($http){
         var service = this;
 
         service.getMatchedMenuItems = function(searchTerm){
-            return $http().then(function(result){
-               var foundItems;
+            return $http({
+                url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+            }).then(function(result){
+                var foundItems = [];
+                var menuItems =  result.data.menu_items;
+                for(var i = 0; i < menuItems.length; i++){
+                    var regex = new RegExp('' + searchTerm +'');
+                    if(regex.test(menuItems[i].description)){
+                        foundItems.push(menuItems[i]);
+                    }
 
-               return foundItems;
+
+                }
+                return foundItems;
             });
         }
     }
 
     function FoundItems(){
         var ddo = {
-            template: ''
+            scope: {
+                items:'<foundItems',
+                onRemove: '&'
+            },
+            restrict: 'E',
+            templateUrl: 'foundItems.html'
         }
 
         return ddo;
